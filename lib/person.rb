3aaedd8c874e49521bfe7a844b2857358d5e5374
HectorLopez1998 <hectorlopezp1998@gmail.com>
2020-12-10 22:43:48 -0500

    commit tree 1dde940bf46bfb8456ae09090da27dc028a34433

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
-  #your code here
+
+    attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+    :weight, :handed, :complexion, :t_shirt_size, 
+    :wrist_size, :glove_size, :pant_length, :pant_width 
+
+    def initialize(attributes_hash)
+@name = attributes_hash[:name]
+@birthday = attributes_hash[:birthday]
+@hair_color = attributes_hash[:hair_color]
+@eye_color = attributes_hash[:eye_color]
+@height = attributes_hash[:height]
+@weight = attributes_hash[:weight]
+@complexion = attributes_hash[:complexion]
+@handed = attributes_hash[:handed]
+@t_shirt_size = attributes_hash[:t_shirt_size]
+@wrist_size = attributes_hash[:wrist_size]
+@glove_size = attributes_hash[:glove_size]
+@pant_length = attributes_hash[:pant_length]
+@pant_width = attributes_hash[:pant_width]
+
+    end
 end
